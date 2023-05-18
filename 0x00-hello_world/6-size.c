9075010a4f30d7eeb7516e859da66b4e80f605fa
Daniel Makai <danielmakai92@gmail.com>
2023-05-18 12:54:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,18 @@
+#include <stdio.h>
+
 /**
-*main - Etry point
-*Return: Always 0 (Success)
-*/
-#include<stdio.h>
+ *main - prints size of various types
+ *
+ *Return: always 0 (Success)
+ */
+
 int main(void)
 {
-	int a;
-	char b;
-	long int c;
-	long long int d;
-	float f;
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-	printf("Size of a char:%lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu  byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s))\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s))\n", (unsigned long)sizeof(f));
 	return (0);
 }
