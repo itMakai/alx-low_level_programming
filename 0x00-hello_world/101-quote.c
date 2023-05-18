1ad7de47c084a6b45bbcfae2b8f23627a464ef2f
Daniel Makai <danielmakai92@gmail.com>
2023-05-18 13:04:38 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - simplicity
+ *
+ *Return: 1
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      stderr);
+
+	return (1);
+}
