4a0a66e53563dbc33eec99dc575baa42e0dbf8bc
Daniel Makai <danielmakai92@gmail.com>
2023-05-18 07:08:39 -0700

    compile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main(void)
 	/**
-	 * main - entry point
+	 * main - must return void agurment and not any other
+	 * void - void value
 	 * return - always 0 (succes)
+	 * 0 - the number to be returned
 	 */
 {
-	/**
-	 * excution
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
