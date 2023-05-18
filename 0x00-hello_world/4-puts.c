109303980075963a74f9ffbb5f902231626dcc2d
Daniel Makai <danielmakai92@gmail.com>
2023-05-18 07:31:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 #include<stdio.h>
 int main(void)
-	/**
-	 * main - must return void agurment and not any other
-	 * void - void value
-	 * return - always 0 (succes)
-	 * 0 - the number to be returned
-	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
