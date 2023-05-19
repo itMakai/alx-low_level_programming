f2d3e013b46574cbe1be13fbb384931bedb2e619
Daniel Makai <danielmakai92@gmail.com>
2023-05-19 13:35:25 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	int m;
+
+	while (n = 48 && n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m = 97 && m <= 102)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
