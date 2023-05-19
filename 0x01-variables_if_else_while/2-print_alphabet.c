930f28358e75f8c401913594ac1fa97120a699d7
Daniel Makai <danielmakai92@gmail.com>
2023-05-19 09:31:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/**
+* main - Entry Point
+* Return: Always 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
