bf1e7fa8bacfcf655556637f600192669c8b4923
Daniel Makai <danielmakai92@gmail.com>
2023-05-19 13:02:44 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+#include<stdio.h>
+int main(void)
+{
+	int n = 122;
+
+	while ( n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
